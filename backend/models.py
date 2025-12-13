a38ab1083e693da009fb629a96612285d77997fe
Aji Purnama <ajipurnama798@gmail.com>
2025-12-13 21:59:52 +0700

    upload project backend pemula

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name     : str
+    age      : int
+    password : str
+
+class Notes(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    title   : str
+    content: str
+    owner_id: int =Field(foreign_key="user.id")
